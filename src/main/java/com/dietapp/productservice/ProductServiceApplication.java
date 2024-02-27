e1808e6638dee3400d4f89b0804b2b3c7f5359b8
Dawid <maciak.dawid.96@gmail.com>
2024-02-27 09:57:07 +0100

    Add PostgreSQL connection config with credentials encryption

--- src/main/java/com/dietapp/productservice/ProductServiceApplication.java
+++ src/main/java/com/dietapp/productservice/ProductServiceApplication.java
@@ -1,9 +1,11 @@
 package com.dietapp.productservice;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
